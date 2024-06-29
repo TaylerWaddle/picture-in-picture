83c5e95b6c598b9fdaf87e18e36da654f83e1fba
Tayler Waddle <taylerwaddle@gmail.com>
2024-06-29 13:06:07 -0600

    Added Button PictureInPicture onclick

--- script.js
+++ script.js
@@ -18,6 +18,13 @@ async function selectMediaStream() {
   }
 }
 
-button.addEventListener();
+button.addEventListener("click", async () => {
+  // Disable button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+});
 
 selectMediaStream();
